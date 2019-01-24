d37bc034fb1e3da50aed7255e64d8b3911e18898
Jun Feng <alexfengau@gmail.com>
2019-01-24 21:57:11 +1100

    initial commit to setup project

---
+++ src/main/java/au/com/skiddoo/shopping/Price.java
@@ -0,0 +1,12 @@
+// Copyright 2015 Skiddoo Pty Ltd
+
+package au.com.skiddoo.shopping;
+
+/**
+ * @author jaiew
+ */
+public class Price {
+
+	// Implement the price object;
+
+}
---
+++ src/test/java/au/com/skiddoo/shopping/CheckoutRegisterTests.java
@@ -0,0 +1,18 @@
+// Copyright 2015 Skiddoo Pty Ltd
+
+package au.com.skiddoo.shopping;
+
+import org.junit.Test;
+
+/**
+ * @author jaiew
+ */
+public class CheckoutRegisterTests {
+
+	@Test
+	public void test() {
+		// throw new UnsupportedOperationException("Tests need to be
+		// implemented");
+	}
+
+}
